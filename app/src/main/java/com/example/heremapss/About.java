8cb700727c3ee2870e66ebbdc86932badfeb5937
Thomas <thomasrossouw@gmail.com>
2022-11-11 13:13:00 +0200

    Android Application using HereMaps

--- app/src/main/java/com/example/heremapss/About.java
+++ app/src/main/java/com/example/heremapss/About.java
@@ -3,6 +3,10 @@ package com.example.heremapss;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,7 +18,9 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
+        EditText edit1=(EditText) findViewById(R.id.edit1);
+        EditText edit2=(EditText) findViewById(R.id.edit2);
+        Button btn=(Button) findViewById(R.id.button);
         // initialize navigation View
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         //set Home Selected
@@ -42,6 +48,25 @@ public class About extends AppCompatActivity {
                 return false;
             }
 
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Intent.ACTION_SEND);
+                i.setType("message/html");
+                i.putExtra(android.content.Intent.EXTRA_EMAIL,new String[] { "ST10121910@vcconnect.edu.za" });
+                i.putExtra(Intent.EXTRA_SUBJECT,"Feedback From App");
+                i.putExtra(Intent.EXTRA_TEXT,"Name:"+edit1.getText()+"\n Message:"+edit2.getText());
+                try {
+                    startActivity(Intent.createChooser(i,"Please select Email"));
+                }
+
+                catch (android.content.ActivityNotFoundException ex)
+                {
+                    Toast.makeText(About.this, "There are no Email Clients", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
